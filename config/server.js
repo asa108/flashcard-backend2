d6e09f803651c31b8cba1003caac9acd55a3bbf6
Asane <yaradayo317@gmail.com>
2022-02-23 16:56:01 -0500

    set up database

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '2fa7da7135e3f2f105c9006ccf5ecc67'),
-    },
-  },
-});
